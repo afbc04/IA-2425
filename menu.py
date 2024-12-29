5f8235a1bdeb4c49756682526ab856e2e5a5ca4e
Sofia Oliveira <asro8589@gmail.com>
2024-12-29 00:15:40 +0000

    Mudança carregar_grafo2

--- menu.py
+++ menu.py
@@ -6,29 +6,31 @@ from meteorologia import Meteorologia
 from condicoesDinamicas import CondicoesDinamicas
 
 # Função para carregar o grafo a partir de um ficheiro JSON na pasta mapa
-# def carregar_grafo2(ficheiro_json="mapa/mapaGrafo.json"):
-#     with open(ficheiro_json, 'r') as f:
-#         dados = json.load(f)
+def carregar_grafo2(ficheiro_json="mapa/mapaGrafo.json"):
+     with open(ficheiro_json, 'r') as f:
+         dados = json.load(f)
 
-#     grafo = Grafo(directed=False)
+     grafo = Grafo(directed=False)
 
-#     # Adicionar nós ao grafo com população e tempo
-#     for no_data in dados["nos"]:
-#         nome = no_data["nome"]
-#         populacao = no_data["populacao"]
-#         tempo = no_data["tempo"]
-#         no = No(nome, populacao=populacao, janela_tempo=tempo)
-#         grafo.m_nodes.append(no)
-#         grafo.m_graph[nome] = []  # Inicializa a lista de adjacências do nó
+     # Adicionar nós ao grafo com população e tempo
+     for no_data in dados["areas"]:
+         nome = no_data["id"]
+         populacao = no_data["populacao"]
+         tempo = no_data["janela_critica"]
+         no = No(nome, populacao=populacao, janela_tempo=tempo)
+         grafo.m_nodes.append(no)
+         grafo.m_graph[nome] = []  # Inicializa a lista de adjacências do nó
 
-#     # Adicionar arestas ao grafo
-#     for aresta in dados["arestas"]:
-#         origem = aresta["origem"]
-#         destino = aresta["destino"]
-#         peso = aresta["peso"]
-#         grafo.add_edge(origem, destino, peso)
+     # Adicionar arestas ao grafo
+     for aresta in dados["arestas"]:
+         origem = aresta["origem"]
+         destino = aresta["destino"]
+         peso = aresta["distancia"]
+         bloqueada = aresta.get("bloqueado", False)
+         print(f"Processar aresta: {origem} -> {destino}, Peso: {peso}, Bloqueada: {bloqueada}")  # Depuração
+         grafo.add_edge(origem, destino, peso, blocked=bloqueada)
 
-#     return grafo
+     return grafo
 
 
 # Função para carregar o grafo a partir de um ficheiro JSON na pasta mapa
