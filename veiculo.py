b5859de8e308728722ca6f53ede50c6dc6099fae
sara <saralopes3001@gmail.com>
2024-12-30 10:50:52 +0000

    Grafo e veículo que têm em consideração o custo do veículo

--- veiculo.py
+++ veiculo.py
@@ -1,14 +1,17 @@
 class Veiculo:
-    def __init__(self, tipo):
+    def __init__(self, tipo, custo):
 
         self.tipo = tipo # avião, carro, ...
+        self.custo = custo # é mais dispendioso usar um avião do que um carro
 
+    def get_custo(self):
+        return self.custo
 
-
+    def get_tipo(self):
+        return self.tipo
 
 
 
         #self.combustivel_disponivel = combustivel_disponivel
-        #self.custo = custo # custo de usar cada veículo
         #self.velocidade = velocidade
         #self.volume_carga = volume_carga # que pode transportar
